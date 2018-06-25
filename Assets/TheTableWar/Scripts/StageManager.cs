905c930ec8fa5831153b08e8097fa49a40c53b3a
tosh7 <zlia.6.lj.425@gmail.com>
2018-06-26 00:43:35 +0900

    6/25 doesn't work well

--- Assets/TheTableWar/Scripts/StageManager.cs
+++ Assets/TheTableWar/Scripts/StageManager.cs
@@ -3,5 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StageManager : MonoBehaviour {
+    public GameObject floor;
 
+    void Start() {
+        
+    }
+
+    void Update() {
+        if(Input.touchCount > 0) {
+            Instantiate(floor, new Vector3(0, 0, 0), Quaternion.identity );
+            floor.AddComponent<Rigidbody>();
+        }
+    }
 }
