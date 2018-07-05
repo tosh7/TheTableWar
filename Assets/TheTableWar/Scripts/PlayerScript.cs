b7b64a61a889e023a4a8aa32991eb3530bfef32e
tosh7 <zlia.6.lj.425@gmail.com>
2018-07-05 11:27:24 +0900

    got a ray

---
+++ Assets/TheTableWar/Scripts/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("here");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		shot();
+	}
+
+	void shot() {
+		int distance = 10;
+		Vector3 center = new Vector3(Screen.width/2, Screen.height/2, 0);
+		Ray ray = camera.ScreenPointToRay(center);
+		RaycastHit hitInfo;
+
+		if(Physics.Raycast(ray, out hitInfo, distance)) {
+			Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
+			Debug.Log("ok" + hitInfo.collider.name);
+		}
+	}
+}
