43e4c47769c8ebbbec2a68028177738930a68897
Otsu1203 <me207@ahk.jp>
2018-07-11 23:33:44 +0900

    Add timer score etc

--- Assets/TheTableWar/Scripts/PlayerScript.cs
+++ Assets/TheTableWar/Scripts/PlayerScript.cs
@@ -1,32 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.iOS;
 
-public class PlayerScript : MonoBehaviour {
-	public Camera camera;
-	public GameObject stageManager;
-	public PlayerScript ps;
+public class PlayerScript : MonoBehaviour
+{
+    public Camera camera;
+    public StageManager stageManager;
+    public PlayerScript playerScript;
+    // Shotボタンを押した回数
+    public int shotCount = 0;
 
-	// Use this for initialization
-	void Start () {
-		Debug.Log("here");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		// shot();
-	}
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("here");
+    }
 
-	public void shot() {
-		int distance = 10;
-		Vector3 center = new Vector3(Screen.width/2, Screen.height/2, 0);
-		Ray ray = camera.ScreenPointToRay(center);
-		RaycastHit hitInfo;
+    // Update is called once per frame
+    void Update()
+    {
+        // Shot();
+    }
 
-		if(Physics.Raycast(ray, out hitInfo, distance)) {
-			Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
-			Debug.Log("ok" + hitInfo.collider.name);
-			stageManager.GetComponent<UnityEngine.XR.iOS.StageManager>().DestoryObject(hitInfo.collider.gameObject);
-		}
-	}
+    public void Shot()
+    {
+        if (stageManager.seconds <= 10.0f && stageManager.seconds >= 0.0f)
+        {
+            int distance = 10;
+            Vector3 center = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+            Ray ray = camera.ScreenPointToRay(center);
+            RaycastHit hitInfo;
+
+            shotCount++;
+
+            if (Physics.Raycast(ray, out hitInfo, distance))
+            {
+                Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
+                Debug.Log("ok" + hitInfo.collider.name);
+                stageManager.GetComponent<UnityEngine.XR.iOS.StageManager>().DestoryObject(hitInfo.collider.gameObject);
+            }
+        }
+    }
 }
