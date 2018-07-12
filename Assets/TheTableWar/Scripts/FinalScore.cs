e209d27cf80fb9bd7b71f10a1a815ff2fccff769
Otsu1203 <me207@ahk.jp>
2018-07-12 09:00:27 +0900

    Add timer score etc

---
+++ Assets/TheTableWar/Scripts/FinalScore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.iOS;
+
+public class FinalScore : MonoBehaviour
+{
+    public Text playerScoreText;
+    // Use this for initialization
+    void Start()
+    {
+        playerScoreText.text = StageManager.GetFinalScore().ToString();
+    }
+}
