b19425d24c99ad4c37236aff2b8cb4d251e58a08
Satoshi Komatsu <zlia.6.lj.425@gmail.com>
2018-07-18 02:03:31 +0900

    added a scene management

---
+++ Assets/TheTableWar/Scripts/Title.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void toButtle(){
+		SceneManager.LoadScene("Main");
+	}
+
+	public void toRanking(){
+		SceneManager.LoadScene("Ranking");
+	}
+}
