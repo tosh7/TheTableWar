79280416db7203928de04154b3234d0ccc5b3f08
tosh7 <zlia.6.lj.425@gmail.com>
2018-07-19 19:15:27 +0900

    understand how to push online

--- Assets/TheTableWar/Scripts/PushOnline.cs
+++ Assets/TheTableWar/Scripts/PushOnline.cs
@@ -8,25 +8,30 @@ using UnityEngine;
 public class PushOnline : MonoBehaviour {
 
 	private DatabaseReference timeRankDB;
+	//登録する１件データをDictionaryで定義(nameとtime)
+	Dictionary<string, object> itemMap = new Dictionary<string, object> ();
+	//１件データをさらにDictionaryに入れる。キーはノード(bossNo/さっき生成したキー)
+	Dictionary<string, object> map = new Dictionary<string, object> ();
+	public string key;
 
 	// Use this for initialization
 	void Start () {
 		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl ("https://thetablewar-14053.firebaseio.com/");
 		timeRankDB = FirebaseDatabase.DefaultInstance.GetReference ("time ranks");
-		string key = timeRankDB.Child (ToString ()).Push ().Key;
-		//登録する１件データをDictionaryで定義(nameとtime)
-		Dictionary<string, object> itemMap = new Dictionary<string, object> ();
-		itemMap.Add ("name", 12);
-		itemMap.Add ("time", 3);
-		//１件データをさらにDictionaryに入れる。キーはノード(bossNo/さっき生成したキー)
-		Dictionary<string, object> map = new Dictionary<string, object> ();
-		map.Add (ToString () + "/" + key, itemMap);
-		//データ更新！
-		timeRankDB.UpdateChildrenAsync (map);
+		//まずbossNoのノードにレコードを登録(Push)して、生成されたキーを取得（これを１件のノード名に使う）
+		key = timeRankDB.Child (ToString ()).Push ().Key;
 	}
 
 	// Update is called once per frame
 	void Update () {
 
+	}
+
+	public void Register() {
+		itemMap.Add ("name", 12);
+		itemMap.Add ("time", 3);
+		map.Add (ToString () + "/" + key, itemMap);
+		//ここでpushしてる
+		timeRankDB.UpdateChildrenAsync (map);
 	}
 }
