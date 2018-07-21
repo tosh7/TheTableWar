7099ddbb195e79fcf2cb203e1f9262be1799a5ff
Satoshi Komatsu <zlia.6.lj.425@gmail.com>
2018-07-21 22:37:05 +0900

    need to fixed a memory warning

--- Assets/TheTableWar/Scripts/ButtonManager.cs
+++ Assets/TheTableWar/Scripts/ButtonManager.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour {
-    public void BackButton()
-    {
-        SceneManager.LoadScene("Title");
+    public void BackButton () {
+        SceneManager.LoadScene ("Title");
+        // SceneManager.UnloadSceneAsync("score");
     }
 
-    public void RegisterButton()
-    {
-        SceneManager.LoadScene("Ranking");
+    public void RegisterButton () {
+        SceneManager.LoadScene ("Ranking");
+        // SceneManager.UnloadSceneAsync("score");
     }
 }
