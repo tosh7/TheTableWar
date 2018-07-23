194abf9b4e9b178a6928588582667fdb807680ef
tosh7 <zlia.6.lj.425@gmail.com>
2018-07-23 15:12:33 +0900

    successed with getting scores from firebase

--- Assets/TheTableWar/Scripts/RankingManager.cs
+++ Assets/TheTableWar/Scripts/RankingManager.cs
@@ -9,43 +9,51 @@ public class RankingManager : MonoBehaviour {
 
 	// Use this for initialization
 	private DatabaseReference timeRankDB;
+	ArrayList leaderBoard = new ArrayList ();
+	private const int MaxScores = 5;
 	void Start () {
 		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl ("https://thetablewar-14053.firebaseio.com/");
 		timeRankDB = FirebaseDatabase.DefaultInstance.GetReference ("time ranks");
 
-		//bossNoのノードからtimeで昇順ソートして最大10件を取る（非同期)
-		timeRankDB.Child("EventSystem(PushOnline)").OrderByChild ("time").LimitToFirst (1).GetValueAsync ().ContinueWith (task => {
-			Debug.Log ("hey");
-			if (task.IsFaulted) { //取得失敗
-				//Handle the Error
-				Debug.Log ("error");
-			} else if (task.IsCompleted) { //取得成功
-				Debug.Log ("success");
-				DataSnapshot snapshot = task.Result; //結果取得
-				Debug.Log(snapshot);
-				IEnumerator<DataSnapshot> en = snapshot.Children.GetEnumerator (); //結果リストをenumeratorで処理
-				int rank = 0;
-				Debug.Log(en);
+		leaderBoard.Clear ();
+		leaderBoard.Add ("Firebase Top " + MaxScores.ToString () + " Scores");
 
-				while (en.MoveNext ()) { //１件ずつ処理
-					Debug.Log (1);
-					// DataSnapshot data = en.Current; //データ取る
-					Debug.Log (2);
-					string name = (string) snapshot.Child ("name").GetValue (true); //名前取る
-					string time = (string) snapshot.Child ("time").GetValue (true); //時間を取る
-					Debug.Log (3);
-					//順位のuGUIに値を設定
-					// GameObject rankItem = rankList.transform.GetChild (rank).gameObject;
-					// TimeRank timeRank = rankItem.GetComponent<TimeRank> ();
-					// timeRank.SetText (rank + 1, name, getTimeStr (time)); //順位1位から
-					rank++;
-					Debug.Log (time);
-				}
-			}
-		});
+		StartListener ();
+
 	}
 
-	
+	protected void StartListener () {
+
+		FirebaseDatabase.DefaultInstance
+			.GetReference ("Leaders").OrderByChild ("score")
+			.ValueChanged += (object sender2, ValueChangedEventArgs e2) => {
+
+				if (e2.DatabaseError != null) {
+
+					Debug.LogError (e2.DatabaseError.Message);
+					return;
+				}
+				Debug.Log ("Received values for Leaders.");
+				string title = leaderBoard[0].ToString ();
+				leaderBoard.Clear ();
+				leaderBoard.Add (title);
+				if (e2.Snapshot != null && e2.Snapshot.ChildrenCount > 0) {
+					foreach (var childSnapshot in e2.Snapshot.Children) {
+						if (childSnapshot.Child ("score") == null ||
+							childSnapshot.Child ("score").Value == null) {
+							Debug.LogError ("Bad data in sample.  Did you forget to call SetEditorDatabaseUrl with your project id?");
+							break;
+						} else {
+							Debug.Log ("Leaders entry : " +
+								childSnapshot.Child ("email").Value.ToString () + " - " +
+								childSnapshot.Child ("score").Value.ToString ());
+							leaderBoard.Insert (1, childSnapshot.Child ("score").Value.ToString () +
+								"  " + childSnapshot.Child ("email").Value.ToString ());
+						}
+					}
+				}
+			};
+	}
 
 	// Update is called once per frame
 	void Update () {
